e2560363ec7bec30c8a91b04b0a5920e87c5b25e
kabirfaisal1 <md.kabirfaisal@outlook.com>
2022-09-11 16:28:34 -0400

    Fixed Bug on GetAPI

--- assets/js/mapScript.js
+++ assets/js/mapScript.js
@@ -5,18 +5,41 @@ var zipEl=document.querySelector("#zip-search-input") ;
 var radiusLocationEl = document.querySelector("#radius-search-input");
 var searchEl = document.querySelector("#btnSearch");
 
+/*function getCurrentLocation_radius (){
+        //this function will be used to get users location
+    var userSubmission = {
+        current_location: myCurrentLocationEl.value.trim(),
+        intersted_radius: radius.value.trim()
+    }
+
+    var itemKey="key??"; //this is the item key I have to get vaule from
+
+   //if local storage do have data continue
+   if(localStorage.getItem(itemKey) != null){
+    console.log(`inside getting currentLocation and radius`);
+
+    myCurrentLocation = localStorage.getItem(userSubmission.current_location);
+    radius = localStorage.getItem(userSubmission.intersted_radius);
+ 
+}else{ //if no data was found display
+    console.log(`localStorage did not have data for ${itemKey}`);
+    innerHighScoreEl.innerText= null;
+}
+}*/
+
+
 
 searchEl.addEventListener("click", ()=>{
     getCurrentLocationAPI();
+    
 })
-/*var currentLocation = null;
-var radius = null ;*/
+
 
 function getCurrentLocationAPI(){
-    var userCurrentPerm = `${cityEl.value.trim()},${stateEl.value.trim()},${zipEl.value.trim()}`
+
+     userCurrentPerm = `${cityEl.value.trim()},${stateEl.value.trim()},${zipEl.value.trim()}`
     console.log(`the Script is inside getCurrentLocationAPI to get the current ${userCurrentPerm}`);
-    localStorage.setItem("UserCurrentLocation", JSON.stringify(userCurrentPerm));
-   //var requestUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=${apiKey.mapQuestKey}&location=${userCurrentPerm}`;
+    //var requestUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=${apiKey.mapQuestKey}&location=${userCurrentPerm}`;
    var requestUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=vWzHFILMQOPgQjlt4C8DWFxfHDsrfaPR&location=${userCurrentPerm}`;
 
 
@@ -29,7 +52,6 @@ function getCurrentLocationAPI(){
           var listItem = document.createElement('li');
           listItem.textContent = data[i].html_url;
           repoList.appendChild(listItem);
-          console.log(repoList.appendChild(listItem));
         }
       });
 }
