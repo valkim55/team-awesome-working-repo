0011eb3b3d0f1c5754fcc039c0e8e45de094b075
kabirfaisal1 <md.kabirfaisal@outlook.com>
2022-09-11 20:28:19 -0400

    script is up to date

--- assets/js/mapScript.js
+++ assets/js/mapScript.js
@@ -1,43 +1,40 @@
 
-var cityEl = document.querySelector("#city-search-input");
-var stateEl=document.querySelector("#state-search-input") ;
-var zipEl=document.querySelector("#zip-search-input") ;
+var currentEL = document.querySelector("#current-search-input");
+
 var radiusLocationEl = document.querySelector("#radius-search-input");
 var searchEl = document.querySelector("#btnSearch");
 
 var latitude = null;
-var longitude =null;
+var longitude = null;
 
-searchEl.addEventListener("click", ()=>{
-  latitude =null; longitude=null;
-    getCurrentLocationAPI();
-    
+
+searchEl.addEventListener("click", () => {
+  latitude = null; longitude = null;
+  getCurrentLocationAPI();
+
 })
 
-function getCurrentLocationAPI(){
+function getCurrentLocationAPI() {
+  //this function will post users current location and return the its coordinates 
 
-     userCurrentPerm = `${cityEl.value.trim()},${stateEl.value.trim()},${zipEl.value.trim()}`
-    console.log(`the Script is inside getCurrentLocationAPI to get the current ${userCurrentPerm}`);
-   var requestUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=vWzHFILMQOPgQjlt4C8DWFxfHDsrfaPR&street=${userCurrentPerm}`;
+  console.log(`the Script is inside getCurrentLocationAPI to get the current ${currentEL.value.trim()}`);
+  var requestUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=vWzHFILMQOPgQjlt4C8DWFxfHDsrfaPR&street=${currentEL.value.trim()}`;
+  fetch(requestUrl, { method: 'GET' }) //fetaching all realted area for current location
+    .then((response) => response.json())
+    .then((data) => {
+      for (var i = 0; i < data.results.length; i++) {
+        console.log("inside the loop to get latitude and longitude of user input");
+        latitude = JSON.stringify(data.results[i].locations[i].latLng['lat']);
+        longitude = JSON.stringify(data.results[i].locations[i].latLng['lng']);
+      }
+      console.log(`Current location latitude is ${latitude} and longitude is ${longitude}`);
+      return latitude && longitude;
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      return error; 
+    });
    
-
-
-    fetch(requestUrl,{method:'GET'}) //fetaching all realted area for current location
-      .then((response) => response.json())
-      .then((data) => {
-     
-    for(var i=0; i < data.results.length; i++){
-          console.log("inside the loop");
-          latitude = JSON.stringify(data.results[i].locations[i].latLng['lat']);
-          longitude = JSON.stringify(data.results[i].locations[i].latLng['lng']);
-
-        }
-        console.log(`Current location latitude is ${latitude} and longitude is ${longitude}`);
-      })
-   
-      .catch((error) => {
-        console.error('Error:', error);
-      });
 }
 
 
