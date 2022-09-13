9f3450d5d9cc884e1cc59fabdc0fa6d28fbdd3e7
kabirfaisal1 <md.kabirfaisal@outlook.com>
2022-09-13 10:54:08 -0400

    have to get the radius and activites around it.

--- assets/js/mapScript.js
+++ assets/js/mapScript.js
@@ -5,12 +5,14 @@ var radiusLocationEl = document.querySelector("#radius-search-input");
 var searchEl = document.querySelector("#btnSearch");
 
 
-var latitude; 
-var longitude; 
+//var latitude; 
+  // var longitude; 
 
 searchEl.addEventListener("click", () => {
    getCurrentLocationAPI(); //get CurrentLocation latitude and longitude 'might not needed when getUserPOIS is build'
-   getUserPOIS(latitude,longitude); //get POIS based on latitude and longitude
+   
+   // getUserPOIS(latitude,longitude); //get POIS based on latitude and longitude
+  // getTOMUserPOIS();
 })
 
 
@@ -23,8 +25,9 @@ function getCurrentLocationAPI() {
     .then((currentLocdata) => {
       for (var i = 0; i < currentLocdata.results.length; i++) {
         console.log("inside the loop to get latitude and longitude of user input");
-        latitude = JSON.stringify(currentLocdata.results[i].locations[i].latLng['lat']); //getting the latitude of users current location
-        longitude = JSON.stringify(currentLocdata.results[i].locations[i].latLng['lng']); //getting the longitude of users current location
+       var latitude = JSON.stringify(currentLocdata.results[i].locations[i].latLng['lat']); //getting the latitude of users current location
+       var longitude = JSON.stringify(currentLocdata.results[i].locations[i].latLng['lng']); //getting the longitude of users current location
+       getTOMUserPOIS(latitude,longitude);
       }
       console.log(`getCurrentLocationAPI: Current location latitude is ${latitude} and longitude is ${longitude}`);
     return latitude && longitude; //returning latitude && longitude and updateing them from null to have vaule from json
@@ -37,7 +40,7 @@ function getCurrentLocationAPI() {
 }
 
 
-function getUserPOIS(latitude,longitude){
+/*function getUserPOIS(latitude,longitude){
  
   console.log(`the Script is inside getUserPOIS to get the current`);
   console.log(`the Script is inside getUserPOIS ${latitude && longitude}`);
@@ -57,5 +60,26 @@ function getUserPOIS(latitude,longitude){
       return error; 
     });
 
-}
+}*/
 
+function getTOMUserPOIS(latitude,longitude){
+      //this will get POIS based from TomTom map
+  console.log(`the Script is inside getUserPOIS to get the current`);
+  console.log(`the Script is inside getUserPOIS ${latitude && longitude}`);
+  var poisRequestUrl = `https://api.tomtom.com/search/2/categorySearch/Beach.json?limit=5&lat=${latitude}&lon=${longitude}&radius=160934&view=Unified&relatedPois=off&key=LgN742cN8MR1QMntpr5PgYtQih7dxeGz`;
+  //-TO-DO-
+  fetch(poisRequestUrl, { method: 'GET' }) //fetaching all realted area for current location which user might be Interest in
+    .then((response) => response.json())
+    .then((data) => {
+      for (var i = 0; i < data.results.length; i++) {
+          //-TO-DO-: after I get the data I will store it in an array for and display in the UI
+        console.log("inside the loop to get latitude and longitude of user input POI" + data.results[i]);
+ 
+      }
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      return error; 
+    });
+
+}
