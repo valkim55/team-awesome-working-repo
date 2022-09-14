389afc31e9793d126755d15639a795037ebb50b2
kabirfaisal1 <md.kabirfaisal@outlook.com>
2022-09-14 09:56:14 -0400

    Made edit on destination html for ID and added error handling msg and added style for errorMsg class. added the destination script to verify the input and for the calendar

--- assets/js/destinationScript.js
+++ assets/js/destinationScript.js
@@ -1,11 +1,18 @@
-var currentEL = document.querySelector("#current-search-location");
-
+var currentEL = document.querySelector("#city-search-input");
 var radiusLocationEl = document.querySelector("#radius-search-dropdown");
 var searchEl = document.querySelector("#location-submit");
+var calendarEL =document.querySelector("#calendar")
 
+var errorEl = document.querySelector(".errorMessage")
 
 searchEl.addEventListener("click", () => {
-   getCurrentLocationAPI(); //get CurrentLocation latitude and longitude 'might not needed when getUserPOIS is build'
+ console.log(`inside searchEl.addEventListener`)
+  if(currentEL.value != ""){                      //verifying that CurrentLocation have vaule
+    errorEl.style.setProperty("visibility", "hidden");
+    getCurrentLocationAPI();
+  }else{
+    errorEl.style.setProperty("visibility", "visible");
+  }
 })
 
 
@@ -75,3 +82,51 @@ function getTOMUserPOIS(latitude,longitude){
     });
 
 }
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    const e = event || window.event;
+
+    if (e.keyCode === 27) { // Escape key
+      closeAllModals();
+    }
+  });
+});
