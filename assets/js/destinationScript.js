713de469e4f0643e172ed2e8378f68522380be47
kabirfaisal1 <md.kabirfaisal@outlook.com>
2022-09-15 09:43:13 -0400

    got the distance convert for the UI and fixed all the  spelling error

--- assets/js/destinationScript.js
+++ assets/js/destinationScript.js
@@ -3,7 +3,7 @@
 var currentEL = document.querySelector("#city-search-input");
 var radiusLocationEl = document.querySelector("#radius-search-dropdown");
 var searchEl = document.querySelector("#location-submit");
-var calendarEL =document.querySelector("#calendar");
+var calendarEL = document.querySelector("#calendar");
 
 //error Selector
 var errorEl = document.querySelector(".errorMessage");
@@ -12,25 +12,25 @@ var radiusErrorEl = document.querySelector("#radiusError");
 var selectingDateErrorEl = document.querySelector("#selectingDateError")
 
 searchEl.addEventListener("click", () => {
- console.log(`inside searchEl.addEventListener`)
+  console.log(`inside searchEl.addEventListener`)
 
- console.log(radiusLocationEl.value); 
-  if(currentEL.value == "" && radiusLocationEl.value == "" /*&& calendarEL.value == "" */){
-  //check if all selector has vaule if not then "visibility" to "visible"
-    errorEl.style.setProperty("visibility", "visible");  
+  console.log(radiusLocationEl.value);
+  if (currentEL.value == "" && radiusLocationEl.value == "" /*&& calendarEL.value == "" */) {
+    //check if all selector has value if not then "visibility" to "visible"
+    errorEl.style.setProperty("visibility", "visible");
   }
-  if(radiusLocationEl.value == ""){
-     //check if radiusLocationEl selector has vaule if not then "visibility" to "visible"
-    radiusErrorEl.style.setProperty("visibility", "visible");  //if CurrentLocation have vaule is "" will set style "visibility" to "visible"
+  if (radiusLocationEl.value == "") {
+    //check if radiusLocationEl selector has value if not then "visibility" to "visible"
+    radiusErrorEl.style.setProperty("visibility", "visible");  //if CurrentLocation have value is "" will set style "visibility" to "visible"
   }
   /*if(calendarEL.value == ""){
-     //check if calendarEL selector has vaule if not then "visibility" to "visible"
-    selectingDateErrorEl.style.setProperty("visibility", "visible");  //if calendarEL have vaule is "" will set style "visibility" to "visible"
+     //check if calendarEL selector has value if not then "visibility" to "visible"
+    selectingDateErrorEl.style.setProperty("visibility", "visible");  //if calendarEL have value is "" will set style "visibility" to "visible"
   }*/
-  if(currentEL.value == ""){
-      //check if currentEL selector has vaule if not then "visibility" to "visible"
-    errorLocationEL.style.setProperty("visibility", "visible");  
-  }else{
+  if (currentEL.value == "") {
+    //check if currentEL selector has value if not then "visibility" to "visible"
+    errorLocationEL.style.setProperty("visibility", "visible");
+  } else {
     errorEl.style.setProperty("visibility", "hidden");
     getCurrentLocationAPI();
   }
@@ -38,26 +38,26 @@ searchEl.addEventListener("click", () => {
 
 function getCurrentLocationAPI() {
   //this function will post users current location and return the its coordinates 
-  
+
   console.log(`the script is inside getCurrentLocationAPI to get the current ${currentEL.value.trim()}`);
   var currentLocrequestUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=vWzHFILMQOPgQjlt4C8DWFxfHDsrfaPR&location=${currentEL.value.trim()}`;
-  fetch(currentLocrequestUrl, { method: 'GET' }) //fetaching all realted area for current location
+  fetch(currentLocrequestUrl, { method: 'GET' }) //fetching all related area for current location
     .then((response) => response.json())
     .then((currentLocdata) => {
       for (var i = 0; i < currentLocdata.results.length; i++) {
         console.log("inside the loop to get latitude and longitude of user input");
-       var latitude = JSON.stringify(currentLocdata.results[i].locations[i].latLng['lat']); //getting the latitude of users current location
-       var longitude = JSON.stringify(currentLocdata.results[i].locations[i].latLng['lng']); //getting the longitude of users current location
-       getTOMUserPOIS(latitude,longitude);
+        var latitude = JSON.stringify(currentLocdata.results[i].locations[i].latLng['lat']); //getting the latitude of users current location
+        var longitude = JSON.stringify(currentLocdata.results[i].locations[i].latLng['lng']); //getting the longitude of users current location
+        getTOMUserPOIS(latitude, longitude);
       }
       console.log(`getCurrentLocationAPI: Current location latitude is ${latitude} and longitude is ${longitude}`);
-    return latitude && longitude; //returning latitude && longitude and updateing them from null to have vaule from json
+      return latitude && longitude; //returning latitude && longitude and updating them from null to have value from json
     })
     .catch((error) => {
       console.error('Error:', error);
-      return error; 
+      return error;
     });
-   
+
 }
 
 
@@ -85,41 +85,41 @@ function getCurrentLocationAPI() {
 
 var locationRespons = []; //store the radius from getTOMUserPOIS Respons
 var limit;
-function getTOMUserPOIS(latitude,longitude){
-      //this will get POIS based from TomTom map
+function getTOMUserPOIS(latitude, longitude) {
+  //this will get POIS based from TomTom map
   console.log(`the Script is inside getUserPOIS to get the current`);
   console.log(`the Script is inside getUserPOIS ${latitude && longitude}`);
-  var convertToKm = radiusLocationEl.value * 1.60934;
-  var km = convertToKm.toString().replace(/[|&;$%@"<>()+,.]/g, "");
-  if(radiusLocationEl.value < 9)
-  {
-    limit = 10-parseInt(radiusLocationEl.value);
-  }else{
+  var convertToMeters = radiusLocationEl.value * 1609.344; //have to convert mile to meters for api radius parameters
+  var meters = convertToMeters.toString().replace(/[|&;$%@"<>()+,.]/g, ""); //have to remove '.' from meters for api radius parameters
+  if (radiusLocationEl.value < 9) {
+    limit = 10 - parseInt(radiusLocationEl.value);
+  } else {
     limit = parseInt(radiusLocationEl.value) + 10;
   }
- 
 
-  var poisRequestUrl = `https://api.tomtom.com/search/2/categorySearch/Beach.json?limit=${parseInt(limit)}&lat=${latitude}&lon=${longitude}&radius=${km}&view=Unified&relatedPois=off&key=LgN742cN8MR1QMntpr5PgYtQih7dxeGz`;
-  //-TO-DO-
-  fetch(poisRequestUrl, { method: 'GET' }) //fetaching all realted area for current location which user might be Interest in
+  
+  var poisRequestUrl = `https://api.tomtom.com/search/2/categorySearch/Beach.json?limit=${parseInt(limit)}&lat=${latitude}&lon=${longitude}&radius=${meters}&view=Unified&relatedPois=off&key=LgN742cN8MR1QMntpr5PgYtQih7dxeGz`;
+  fetch(poisRequestUrl, { method: 'GET' }) //fetching all related area for current location which user might be Interest in
     .then((response) => response.json())
     .then((data) => {
       for (var i = 0; i < data.results.length; i++) {
-          var locationobj ={ //objL to be passed in locationRespons 
-            city: data.results[i].address.localName,
-            state: data.results[i].address.municipality,
-            latLng: [
-                     data.results[i].position.lat,
-                     data.results[i].position.lon
-                    ]
-          }
-          locationRespons.push(locationobj);
+       
+        var locationobj = { //objL to be passed in locationRespons 
+          city: data.results[i].address.localName,
+          state: data.results[i].address.municipality,
+          latLng: [
+            data.results[i].position.lat,
+            data.results[i].position.lon
+          ],
+          distance: `${data.results[i].dist*0.000621371192} mi` //converting the distance back to mile for the UI
+        }
+        locationRespons.push(locationobj);
       }
-   
+        console.log(locationRespons);
     })
     .catch((error) => {
       console.error('Error:', error);
-      return error; 
+      return error;
     });
 
 }
